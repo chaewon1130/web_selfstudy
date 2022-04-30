b187721965494a8a686c327149b7e4617c8d730d
DH <chaewon1130@naver.com>
2022-04-30 21:17:24 +0900

    220430 doit js

---
+++ 220430/variable.js
@@ -0,0 +1,14 @@
+// console.log(num);
+// 호이스팅
+// let num = 13;
+
+// num1 = 3
+// console.log(num1);
+// var num1 = 1;
+
+// const DIVISION = 300;
+// console.log(`DIVISION : ${DIVISION}`);
+// console.log('DIVISION : ' + DIVISION);
+// console.log(`value : ${DIVISION}, type: ${typeof DIVISION}`);
+
+console.log(5!==3);
